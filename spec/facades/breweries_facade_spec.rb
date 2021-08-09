83357290eeba5c5b00586137749fb517e9f4141b
Taija <taija.warbelow@gmail.com>
2021-08-09 08:34:49 -0700

    expect shape of .breweries return

---
+++ spec/facades/breweries_facade_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Breweries Facade' do
+  describe '::breweries' do
+    it 'creates a CityBreweries object', :vcr do
+      city_brews = BreweriesFacade.breweries('denver,co', 5)
+      expect(city_brews).to be_a(CityBreweries)
+      expect(city_brews.location).to be_a(String)
+      expect(city_brews.forecast).to be_a(Hash)
+      city_brews.breweries.each do |brewery|
+        expect(brewery).to be_a(Brewery)
+      end
+    end
+  end
+end

5245cc5f019665147702b3eb957e5760883891ae
Taija <taija.warbelow@gmail.com>
2021-08-08 22:10:53 -0700

    attr reader and initialize

---
+++ app/poros/current.rb
@@ -0,0 +1,16 @@
+class Current
+  attr_reader :datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon
+
+  def initialize(current_weather)
+    @datetime = Time.at(data[:dt]).to_s
+    @sunrise = Time.at(data[:sunrise]).to_s
+    @sunset = Time.at(data[:sunset]).to_s
+    @temperature = data[:temp]
+    @feels_like = data[:feels_like]
+    @humidity = data[:humidity]
+    @uvi = data[:uvi]
+    @visibility = data[:visibility]
+    @conditions = data[:weather].first[:description]
+    @icon = data[:weather].first[:icon]
+  end
+end

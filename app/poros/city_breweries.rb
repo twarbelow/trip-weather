142fc1a4cf65c2774f20ea5f5c230dc3395cf7ef
Taija <taija.warbelow@gmail.com>
2021-08-09 09:57:31 -0700

    id and type

--- app/poros/city_breweries.rb
+++ app/poros/city_breweries.rb
@@ -1,7 +1,9 @@
 class CityBreweries
-  attr_reader :location, :forecast, :breweries
+  attr_reader :id, :type, :location, :forecast, :breweries
 
   def initialize(location, forecast, breweries)
+    @id = nil
+    @type = 'breweries'
     @location = location
     @forecast = {summary: forecast[:current][:weather].first[:description], temperature: "#{forecast[:current][:temp]} F"}
     @breweries = breweries

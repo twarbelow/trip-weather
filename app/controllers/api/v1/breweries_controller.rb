0d2cc6b240bf304776f27aec1e9c6adb71c3a168
Taija <taija.warbelow@gmail.com>
2021-08-09 09:53:19 -0700

    correct typo

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BreweriesController < ApplicationController
   def index
-    breweries = BreweriesFacade(params[:location, params[:quantity]])
+    breweries = BreweriesFacade.breweries(params[:location], params[:quantity])
     render json: BreweriesSerializer.new(breweries)
   end
 end

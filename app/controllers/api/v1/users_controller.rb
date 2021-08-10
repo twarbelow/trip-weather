3ecb38b974d8ada91fc732e67a163248eac01cb0
Taija <taija.warbelow@gmail.com>
2021-08-10 09:22:53 -0700

    remove unnecessary instance variable

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     if user_params[:password] == user_params[:password_confirmation]
-      @user = User.new(user_params)
+      user = User.new(user_params)
       if user.save
         render json: UsersSerializer.new(user), status: :created
       else

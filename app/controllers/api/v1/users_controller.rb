b6ccaaa7fd173db866f9b7a3dbdfabd33e02568b
Taija <taija.warbelow@gmail.com>
2021-08-10 08:45:35 -0700

    ::create with logic for bad request

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    if user_params[:password] == user_params[:password_confirmation]
+      @user = User.new(user_params)
+      if user.save
+        render json: UsersSerializer.new(user), status: :created
+      else
+        render status: :bad_request, body: user.errors.full_messages.to_sentence
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end

d7df99fa7910ed382f0ff6a006b7c67ce42572a9
Taija <taija.warbelow@gmail.com>
2021-08-09 21:53:14 -0700

    complete spec, spec passing

--- spec/services/background_service_spec.rb
+++ spec/services/background_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'BackgroundService' do
   it 'Returns parsed JSON responses for the Unsplash image API', :vcr do
-    data = BackgroundService.city_image_search('denver,co')
+    data = BackgroundService.get_city_image('denver,co')
 
     expect(data).to be_a(Hash)
     expect(data).to have_key(:results)
@@ -12,6 +12,6 @@ describe 'BackgroundService' do
     expect(data[:results].first[:user]).to have_key(:name)
     expect(data[:results].first[:user][:name]).to be_a(String)
     expect(data[:results].first[:urls]).to be_a(Hash)
-    expect(data[:results].first[:urls].heys).to include(:raw, :full, :portfolio_url)
+    expect(data[:results].first[:urls]).to have_key(:full)
   end
 end

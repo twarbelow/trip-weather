8ba8654db29a1e17fe0c1677f6f8048ed604873b
Taija <taija.warbelow@gmail.com>
2021-08-09 22:27:05 -0700

    background fixture, correct expectation

--- spec/facades/background_facade_spec.rb
+++ spec/facades/background_facade_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Background Facade' do
   describe '::search_for_background' do
     it 'creates a Background object', :vcr do
-      background = BackgroundFacade.get_city_image('denver,co')
+      background = BackgroundFacade.search_for_background('denver,co')
 
       expect(background).to be_a(Background)
       expect(background.image_url).to be_a(String)
-      expect(background.author).to be_a(String)
+      expect(background.location).to be_a(String)
+      expect(background.credit).to be_a(Hash)
       expect(background.description).to be_a(String)
     end
   end

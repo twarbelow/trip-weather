7a2d5c195e780972ab7f74db31d619597161869e
Taija <taija.warbelow@gmail.com>
2021-08-09 22:23:37 -0700

    final spec, passing

--- spec/requests/api/v1/get_background_image_for_city_spec.rb
+++ spec/requests/api/v1/get_background_image_for_city_spec.rb
@@ -9,15 +9,16 @@ RSpec.describe '/api/v1/backgrounds?' do
     expect(result).to have_key(:data)
     expect(result[:data].keys).to include(:id, :type)
     expect(result[:data][:id]).to eq(nil)
-    expect(result[:data][:type]).to eq('image')
+    expect(result[:data][:type]).to eq('background')
 
     expect(result[:data]).to have_key(:attributes)
     expect(result[:data][:attributes]).to be_a(Hash)
-    expect(result[:data][:attributes].keys).to include(:image_url, :description, :author, :source, :author_profile)
+    expect(result[:data][:attributes].keys).to include(:image_url, :description, :credit)
     expect(result[:data][:attributes][:image_url]).to be_a(String)
     expect(result[:data][:attributes][:description]).to be_a(String)
-    expect(result[:data][:attributes][:author]).to be_a(String)
-    expect(result[:data][:attributes][:source]).to be_a(String)
-    expect(result[:data][:attributes][:author_profile]).to be_a(String)
+    expect(result[:data][:attributes][:credit]).to be_a(Hash)
+    expect(result[:data][:attributes][:credit].keys).to include(:source, :author)
+    expect(result[:data][:attributes][:credit][:source]).to be_a(String)
+    expect(result[:data][:attributes][:credit][:author]).to be_a(String)
   end
 end

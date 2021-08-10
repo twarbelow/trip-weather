c915731d0839df0760ae70bdc5287f973a8d2cd7
Taija <taija.warbelow@gmail.com>
2021-08-10 10:14:22 -0700

    happy path for user login

---
+++ spec/requests/api/v1/post_user_login_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe '/api/v1/sessions' do
+  it 'creates a new session when a user logs in with proper credentials' do
+    user = create(user, password: 'password')
+    body_data = {
+                  email: user.email,
+                  password: user.password,
+                }
+
+    headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+
+    post '/api/v1/sessions', headers: headers, params: JSON.generate(body_data)
+
+    expect(response).to eq(200)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:data)
+    expect(parsed[:data].keys).to include(:type, :id, :attributes)
+    expect(parsed[:data][:type]).to eq('users')
+    expect(parsed[:data][:id]).to eq(user.id.to_s)
+    expect(parsed[:data][:attributes].keys).to include(:email, :api_key)
+    expect(parsed[:data][:attributes][:email]).to eq(user.email)
+    expect(parsed[:data][:attributes][:api_key]).to eq(user.api_key)
+  end
+end

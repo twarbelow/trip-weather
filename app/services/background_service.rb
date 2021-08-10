315281561e6f98149e8c4b251f5a764f5376cafc
Taija <taija.warbelow@gmail.com>
2021-08-09 21:53:48 -0700

    ::get_city_image

---
+++ app/services/background_service.rb
@@ -0,0 +1,16 @@
+class BackgroundService
+  def self.get_city_image(location)
+    response = conn.get('/search/photos') do |req|
+        req.params['client_id'] = ENV['BACKGROUND_API_KEY']
+        req.params['query'] = "city of #{location}"
+        req.params['per_page'] = 1
+      end
+      JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(url: 'https://api.unsplash.com')
+  end
+end

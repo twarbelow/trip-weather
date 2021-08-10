8f2201b6bf2e5768f6cb0056f88d13c18993e551
Taija <taija.warbelow@gmail.com>
2021-08-10 16:29:00 -0700

    correct parameters that are sent to get_forecast

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,7 +1,7 @@
 class RoadTripFacade
   def self.create_trip(origin, destination)
     travel_time = LocationFacade.calculate_travel_time(origin, destination)
-    forecast = WeatherService.get_forecast(travel_time[:locations][1][:latLng])
+    forecast = WeatherService.get_forecast(travel_time[:route][:locations][1][:latLng][:lat], travel_time[:route][:locations][1][:latLng][:lng])
     RoadTrip.new({origin: origin, destination: destination}, {human_time: travel_time[:formattedTime], seconds: travel_time[:time]}, forecast)
   end
 end

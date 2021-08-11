b38a8d4e3da8b11a0927a5367936ce4e3666aa27
Taija <taija.warbelow@gmail.com>
2021-08-10 18:08:28 -0700

    correct typo in destination

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'RoadTrip Facade' do
   describe '::create_trip' do
     it 'creates a Roadtrip object', :vcr do
-      params = {origin: 'denver,co', desination: 'pueblo,co'}
+      params = {origin: 'denver,co', destination: 'pueblo,co'}
       trip = RoadTripFacade.create_trip(params[:origin], params[:destination])
 
       expect(trip).to be_a(Roadtrip)

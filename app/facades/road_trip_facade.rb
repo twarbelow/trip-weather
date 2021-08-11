b7cf1b700e3a999e09108694a7a7e5e3d517d2de
Taija <taija.warbelow@gmail.com>
2021-08-10 17:31:00 -0700

    format for clarity
    correct lat lng

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,7 +1,10 @@
 class RoadTripFacade
   def self.create_trip(origin, destination)
     travel_time = LocationFacade.calculate_travel_time(origin, destination)
-    forecast = WeatherService.get_forecast(travel_time[:route][:locations][1][:latLng][:lat], travel_time[:route][:locations][1][:latLng][:lng])
-    RoadTrip.new({origin: origin, destination: destination}, {human_time: travel_time[:formattedTime], seconds: travel_time[:time]}, forecast)
+    forecast = WeatherService.get_forecast(travel_time[:route][:locations][1][:latLng][:lat],
+                                           travel_time[:route][:locations][1][:latLng][:lng])
+    RoadTrip.new({origin: origin, destination: destination},
+                 {human_time: travel_time[:route][:formattedTime], seconds: travel_time[:route][:time]},
+                 forecast)
   end
 end

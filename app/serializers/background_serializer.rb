0c5a1f867d3b5c9c6ed41cd9f46787c8a9c128e5
Taija <taija.warbelow@gmail.com>
2021-08-11 10:56:07 -0700

    remove unnecessary id from attributes

--- app/serializers/background_serializer.rb
+++ app/serializers/background_serializer.rb
@@ -1,5 +1,6 @@
 class BackgroundSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :location, :description, :image_url, :credit
+  attributes :location, :description, :image_url, :credit
+  set_id {nil}
 end
